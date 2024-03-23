4a9cc6b3f0fa6998974a0d1f7fb0773ed4aab5fd
Jim Jenkinson <jamesjenkinson10@gmail.com>
2024-03-23 22:35:53 +0000

    landing page

--- src/presentation/LandingPage/pages/LandingPage.js
+++ src/presentation/LandingPage/pages/LandingPage.js
@@ -1,15 +1,106 @@
-import { Stack } from "@mui/material";
-import React, { useContext } from "react";
+import { Stack, Box, Button, TextField } from "@mui/material";
+import React, { useState } from "react";
 
-import { GlobalContext } from "../../../contexts/GlobalContext";
+import { register, login } from "../../../utils/apiEndpoints";
+
+import "./LandingPage.css";
 
 const LandingPage = () => {
-  const { user } = useContext(GlobalContext);
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [message, setMessage] = useState(null);
+
+  const handleClick = async () => {
+    if (isRegistering) {
+      const res = await register(email, firstName, lastName);
+
+      setMessage(res.message);
+      setIsRegistering(!isRegistering);
+    } else {
+      const res = await login(email);
+
+      setMessage(res.message);
+      setIsRegistering(!isRegistering);
+    }
+  };
+
   return (
-    <>
-      <Stack>landing page</Stack>
-      {/* <Footer /> */}
-    </>
+    <Stack className="login-container" height="calc(100vh)">
+      <img
+        className="tiger-logo"
+        src="/images/tiger-face-svgrepo-com.svg"
+        alt="cute face of a tiger"
+      />
+      <Box className="button-container">
+        <TextField
+          label="Email"
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          sx={{
+            width: "90%",
+            alignSelf: "center",
+            height: "60px",
+          }}
+        />
+
+        {isRegistering && !message && (
+          <TextField
+            label="First Name"
+            variant="outlined"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            sx={{ width: "90%", marginBottom: 1 }}
+          />
+        )}
+        {isRegistering && !message && (
+          <TextField
+            label="Last Name"
+            variant="outlined"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            sx={{ width: "90%", marginBottom: 1 }}
+          />
+        )}
+
+        <button
+          className="button-login"
+          variant="contained"
+          color="primary"
+          onClick={handleClick}
+        >
+          {isRegistering ? "Register" : "Login"}
+        </button>
+        <Button
+          variant="text"
+          onClick={() => setIsRegistering(!isRegistering)}
+          sx={{
+            fontSize: "2rem",
+            textTransform: "none",
+            fontWeight: "600",
+            textAlign: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box>
+            {message ? (
+              <>{message}</>
+            ) : (
+              <>
+                {isRegistering ? (
+                  <span style={{ color: "black" }}>or login&nbsp;</span>
+                ) : (
+                  <span style={{ color: "black" }}>or register&nbsp;</span>
+                )}
+                <span>here</span>
+              </>
+            )}
+          </Box>
+        </Button>
+      </Box>
+    </Stack>
   );
 };
 
